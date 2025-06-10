6bfbfc0884fa5290e939269c05ddc7ab5c8ebba3
Sarujan Sk <sarujansk@Sarujans-MacBook-Pro.local>
2025-06-10 21:24:49 +0530

    update

---
+++ for-each-loop/example1/demo.java
@@ -0,0 +1,17 @@
+public class demo {
+    public static void main(String[] args) {
+        // Using a for-each loop to iterate over an array of integers
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int number : numbers) {
+            System.out.println("Number: " + number);
+        }
+
+        // Using a for-each loop to iterate over an array of strings
+        String[] fruits = {"Apple", "Banana", "Cherry"};
+
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+    }
+}
